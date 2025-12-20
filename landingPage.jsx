ca80fdc7310ce71c943369a53912995419904f30
RostislavStaffs <v015989p@student.staffs.ac.uk>
2025-12-20 16:04:48 +0000

    Landing page created
    Started working on the navbar

---
+++ landingPage.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./landing.css";
+
+export default function LandingPage() {
+  return (
+    <div className="page">
+      <header className="navbar">
+        <div className="nav-inner">
+          <div className="logo">
+            Adventure <span>GO</span>
+          </div>
+
+          <nav className="nav-links">
+            <a href="#">Home</a>
+            <a href="#">About us</a>
+            <a href="#">Contact</a>
+          </nav>
+
+          <div className="nav-actions">
+            <a href="#" className="login">Login</a>
+            <button className="signup">Sign up</button>
+          </div>
+        </div>
+      </header>
+
+      <main className="content">
+       
+      </main>
+    </div>
+  );
+}
